fbe59327790e2aa4fb32d80f0a814be3c25ca997
muj-i <mujahedul@iamsheba.xyz>
2025-04-22 13:56:28 +0600

    CLIENT FOLDER NAME UPDATE

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/client/',  // This would break if the app is at the root.
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
-    },
-  },
 });
