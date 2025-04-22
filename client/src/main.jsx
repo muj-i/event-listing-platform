e9ed61f4e53456823a409c3c7c29b2a029b3c6d5
muj-i <mujahedul@iamsheba.xyz>
2025-04-22 14:03:29 +0600

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Or your Tailwind/main styles
+// import './index.css'; // Or your Tailwind/main styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
