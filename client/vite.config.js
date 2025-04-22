fb2f655f15b128e3ca1168210b95c0949fcf0659
muj-i <mujahedul@iamsheba.xyz>
2025-04-22 14:14:09 +0600

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/client/',  // This would break if the app is at the root.
   plugins: [react()],
 });
